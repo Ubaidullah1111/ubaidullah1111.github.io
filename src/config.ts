6ea3517cbfe369d817f3adaafc0f717ad7879d04
Ubaidullah1111 <gaffoorubaidullah@gmail.com>
2026-03-26 20:53:30 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,43 @@ name: "Ubaidullah Gaffoor",
 title: "Engineer",
 description: "I am the laanie",
 accentColor: "#1d4ed8", // Hex color for theme
+
+  social: {
+  email: "ubaidullah.gaf@gmail.com",
+  linkedin: "https://linkedin.com/in/ubaidullahgaffoor", 
+  github: "https://github.com/ubaidullah1111",
+}
+
+skills: ["Java", "C", "C++", "Python", "MATLAB", "Project Management"]
+
+projects: [
+  {
+    name: "Two-wheeled inverted Pendulum Control Robot",
+    description: "Implemented MEMS 3-axis accelorometor gyroscopic stabalisation using a PID controller",
+    link: "https://www.youtube.com/watch?v=Ccj8CW6HGzI",
+    skills: ["MATLAB", "Simulink Simscape Multibody", "Arduino"], // Technologies used
+  }
+]
+
+experience: [
+  {
+    company: "sappi",
+    title: "Engineer in Training",
+    dateRange: "Jan 2025 - Present",
+    bullets: [
+      "Implemented a PILZ safety controller for emergency-stop logic, reducing unplanned downtime and improving line reliability",
+    ],
+  }
+]
+
+education: [
+  {
+    school: "Stellenbosch University",
+    degree: "Bachelor of Engineering in Electrical and Electronics Engineering",
+    dateRange: "2019 - 2024",
+    achievements: [
+      "MSA Education Team Leader",
+      "Residence (Metanoia) Muslim Student Society member",
+    ]
+  }
+]
